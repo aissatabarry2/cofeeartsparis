5d9ec77e544c29dd49a59a921b1afdb17dd9f185
aissatabarry2 <ayebarry28@gmail.com>
2026-06-04 10:16:12 +0000

    feat: déploiement frontend et backend sur Vercel

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -2,9 +2,10 @@ const router = require('express').Router();
 const { getOrders, createOrder, updateOrderStatus, getStats } = require('../controllers/orderController');
 const { protect, adminOnly } = require('../middleware/authMiddleware');
 
+router.get('/stats', protect, adminOnly, getStats);
 router.get('/', protect, getOrders);
 router.post('/', protect, createOrder);
 router.put('/:id', protect, adminOnly, updateOrderStatus);
-router.get('/stats', protect, adminOnly, getStats);
+
 
 module.exports = router;
